7b749a243ca4f60e85dcf39f8a054d5255c48a84
Ferdianto Aditya <44653333+squidmoron@users.noreply.github.com>
2021-07-23 18:14:12 +0700

    Add files via upload

---
+++ tes.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+ID=['F1','F2','F3','F4','F5','F6','F7','F8'] #anggap variable mesin
+val=[32.23,42.12,12.42,21.64,65.87,30.43,10.93,87.02] #anggap variable valuenya
+
+def bubblesort(key,value,index):
+	x=[['',0]]*index #declaring variable x as array
+	for i in range(index) :
+		x[i]=(key[i],value[i])
+
+	dtype = [('key','S3'),('value',float)]
+	a=np.array(x, dtype=dtype)
+	c=np.sort(a, order='value')
+	c=c[::-1]#sort reverse
+	return(c)
+
+result=bubblesort(ID,val,8) #nanti disesuaikan dengan variable yg di inginkan
+for i in range(8) :
+	print(result[i][0].decode("utf-8"),'=',result[i][1])
